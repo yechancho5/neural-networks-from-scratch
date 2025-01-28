eeebf293a252b94101825c41182ca050c69b67b5
yechancho5 <yechancho5@gmail.com>
2025-01-28 04:28:40 -0500

    Added ReLU and Softmax Activation Functions

--- nnfstest.py
+++ nnfstest.py
@@ -13,19 +13,42 @@ class Layer_Dense:
     def forward(self, inputs):
         # Calculate output values from inputs, weights and biases
         self.output = np.dot(inputs, self.weights) + self.biases
+        
+class Activation_ReLU: # f(x) = max(x, 0)
+    # Forward pass
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+class Activation_Softmax: 
+    def forward(self, inputs):
+        # Get unormalized probabilities
+        exp_values=np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        # Divide 
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+
+        self.output = probabilities
 
 X, y = spiral_data(samples=100, classes=3) # Create dataset in the 2D plane (100 samples, 2 features
 
 # Create dense layer with 2 input features and 3 output values
 dense1 = Layer_Dense(2, 3)
 
+activation1 = Activation_ReLU()
+
 # Second "hidden" layer
-dense2 = Layer_Dense(3, 1)
+dense2 = Layer_Dense(3, 3)
+
+activation2 = Activation_Softmax()
 
 # Perform a forward pass of training data through this layer
 dense1.forward(X)
 
-# Perform a forward pass of 1st layer outputs through this layer
-dense2.forward(dense1.output)
+activation1.forward(dense1.output) # Make a forward pass through the activation function 
 
-print(dense2.output[:5]) # Shape of (100, 1)
+# Makes a forward pass through second Dense layer
+dense2.forward(activation1.output)
+
+# Makes a forward pass through the Softmax Activation Function
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
