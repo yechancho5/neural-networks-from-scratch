7f817ef98474257cc2156998b8ed68fe046eb859
yechancho5 <yechancho5@gmail.com>
2025-01-29 13:26:43 -0500

    Added loss and accuracy functions

--- nnfstest.py
+++ nnfstest.py
@@ -28,7 +28,39 @@ class Activation_Softmax:
 
         self.output = probabilities
 
-X, y = spiral_data(samples=100, classes=3) # Create dataset in the 2D plane (100 samples, 2 features
+class Loss: 
+    # Calculates the data and regularization losses
+    def calculate(self, output, y):
+        
+        sample_losses = self.forward(output, y)
+       
+        # Calculate mean loss
+        data_loss = np.mean(sample_losses)
+
+        return data_loss
+
+class Loss_CategoricalCrossentropy(Loss):
+    def forward(self, y_pred, y_true):
+
+        samples = len(y_pred)
+
+        # Clip data to prevent division by 0
+        # Clip both sides to not drag mean towards any value
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        # Probabilities for target values
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+
+        # Mask values - for lists of lists
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped * y_true, axis=1) # possible because one-hot encoded lists are ex) [0, 0, 1], so multiplying by 1 cancels out
+
+        # Losses
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
+
+X, y = spiral_data(samples=100, classes=3) # Create dataset in the 2D plane (100 samples, 2 features)
 
 # Create dense layer with 2 input features and 3 output values
 dense1 = Layer_Dense(2, 3)
@@ -48,7 +80,18 @@ activation1.forward(dense1.output) # Make a forward pass through the activation 
 # Makes a forward pass through second Dense layer
 dense2.forward(activation1.output)
 
-# Makes a forward pass through the Softmax Activation Function
+# Makes a forward pass through the Softmax Activation Function (100, 3) 
 activation2.forward(dense2.output)
 
-print(activation2.output[:5])
+loss_function = Loss_CategoricalCrossentropy()
+loss = loss_function.calculate(activation2.output, y)
+
+print('loss:', loss)
+
+# Calculate accuracy from output of activation2 and targets along the first axis (row)
+predictions = np.argmax(activation2.output, axis=1)
+if len(y.shape) == 2:
+    y = np.argmax(y, axis=1)
+accuracy = np.mean(predictions==y)
+
+print('acc:', accuracy)
