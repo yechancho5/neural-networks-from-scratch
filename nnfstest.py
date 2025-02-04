c522e554906d38f9ccae13bd4de6fa1931138630
yechancho5 <yechancho5@gmail.com>
2025-02-04 03:33:59 -0500

    Added ADAM, adaptive momentum
    The cache holds a history of squared gradients which means the bigger
    the sum of the updates is, the smaller updates are made further in
    training. Adam optimizer also adds a bias correction mechanism. Code is
    based on RMSProp. At each iteration t, for parameters beta_1 and beta_2,
    update bias, and momentum estimates, apply bias and weight corrections,
    update parameters. Loss brought down to 0.073 and acc up to 98.3%

--- nnfstest.py
+++ nnfstest.py
@@ -54,7 +54,7 @@ class Activation_Softmax:
         self.dinputs = np.empty_like(dvalues)
 
         # Enumerate outputs and gradients
-        for index, (single_output, single_dvalues) in enumerate(zip((self.output, dvalues))):
+        for index, (single_output, single_dvalues) in enumerate(zip(self.output, dvalues)):
             single_output = single_output.reshape(-1, 1)
             # Calculate Jacobian matrix
             jacobian_matrix = np.diagflat(single_output) - np.dot(single_output, single_output.T)
@@ -137,20 +137,91 @@ class Activation_Softmax_Loss_CategoricalCrossentropy():
 
 class Optimizer_SGD: 
     # Initialize with a default learning rate of 1
-    def __init__(self, learning_rate=1.0, decay=0):
+    def __init__(self, learning_rate=1.0, decay=0, momentum=0):
         self.learning_rate = learning_rate
         self.current_learning_rate = learning_rate
         self.decay = decay
         self.iterations = 0
+        self.momentum = momentum
 
     def pre_update_params(self):
         if self.decay:
             self.current_learning_rate = self.learning_rate * (1. / (1. + self.decay * self.iterations))
     
     def update_params(self, layer):
-        layer.weights += -self.current_learning_rate * layer.dweights # W = W - learning rate * gradient of loss w.r.t. weights
-        layer.biases += -self.current_learning_rate * layer.dbiases # B = B - learning rate * gradient of loss w.r.t. biases
+        
+        # if momentum is used
+        if self.momentum: 
+
+            # If layer does not contain momentum arrays, create them filled with zeroes
+            if not hasattr(layer, 'weight_momentums'):
+                layer.weight_momentums = np.zeros_like(layer.weights)
+                # If there is no momentum array for weights, there none for biases
+                layer.bias_momentums = np.zeros_like(layer.biases)
+
+            # Build weight updates with momentum using previous update's direction to minimize chances of getting stuck at local min
+            # multiply by retain factor and update w/ current gradients
+            weight_updates = self.momentum * layer.weight_momentums - self.current_learning_rate * layer.dweights
+            layer.weight_momentums = weight_updates
+
+            # Build bias updates
+            bias_updates = self.momentum * layer.bias_momentums - self.current_learning_rate * layer.dbiases
+            layer.bias_momentums = bias_updates
+        # SGD updates (before momentum updates)
+        else:
+            weight_updates = -self.current_learning_rate * layer.dweights # W = W - learning rate * gradient of loss w.r.t. weights
+            bias_updates = -self.current_learning_rate * layer.dbiases # B = B - learning rate * gradient of loss w.r.t. biases
+
+        # Update weights and biases using momentum or vanilla
+        layer.weights += weight_updates
+        layer.biases += bias_updates
     
+    def post_update_params(self):
+        self.iterations += 1
+
+class Optimizer_Adam:
+
+    # Initialize optimizer
+    def __init__(self, learning_rate=0.001, decay=0, epsilon=1e-7,beta_1=0.9,beta_2=0.999):
+        self.learning_rate = learning_rate
+        self.current_learning_rate = learning_rate
+        self.decay = decay
+        self.iterations = 0
+        self.epsilon = epsilon
+        self.beta_1 = beta_1
+        self.beta_2 = beta_2
+
+    def pre_update_params(self):
+        if self.decay:
+            self.current_learning_rate = self.learning_rate * (1 / (1 + self.decay * self.iterations))
+    
+    def update_params(self, layer):
+        # If layer does not contain cache arrays, create them filled with zeros
+        if not hasattr(layer, 'weight_cache'):
+            layer.weight_momentums = np.zeros_like(layer.weights)
+            layer.weight_cache = np.zeros_like(layer.weights)
+            layer.bias_momentums = np.zeros_like(layer.biases)
+            layer.bias_cache = np.zeros_like(layer.biases)
+        
+        layer.weight_momentums = self.beta_1 * layer.weight_momentums + (1 - self.beta_1) * layer.dweights
+        layer.bias_momentums = self.beta_1 * layer.bias_momentums + (1 - self.beta_1) * layer.dbiases
+
+        # Get corrected momentum
+        weight_momentums_corrected = layer.weight_momentums / (1-self.beta_1 ** (self.iterations + 1))
+        bias_momentums_corrected = layer.bias_momentums / (1-self.beta_1 ** (self.iterations + 1))
+
+        # Update cache with squared current gradients
+        layer.weight_cache = self.beta_2 * layer.weight_cache + (1 - self.beta_2) * layer.dweights**2
+        layer.bias_cache = self.beta_2 * layer.bias_cache + (1 - self.beta_2) * layer.dbiases**2
+
+        # Get corrected cache
+        weight_cache_corrected = layer.weight_cache / (1- self.beta_2 ** (self.iterations + 1))
+        bias_cache_corrected = layer.bias_cache / (1 - self.beta_2 ** (self.iterations + 1))
+
+        # Vanilla SGD parameter update + normalization with square rooted cache
+        layer.weights += -self.current_learning_rate * weight_momentums_corrected / (np.sqrt(weight_cache_corrected) + self.epsilon)
+        layer.biases += -self.current_learning_rate * bias_momentums_corrected / (np.sqrt(bias_cache_corrected) + self.epsilon)
+        
     def post_update_params(self):
         self.iterations += 1
 
@@ -168,7 +239,7 @@ dense2 = Layer_Dense(64, 3)
 loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy()
 
 # Create optimizer
-optimizer = Optimizer_SGD(decay=1e-3)
+optimizer = Optimizer_Adam(learning_rate=0.05, decay=5e-7)
 
 for epoch in range(10001):
     # Perform a forward pass of training data through this layer
